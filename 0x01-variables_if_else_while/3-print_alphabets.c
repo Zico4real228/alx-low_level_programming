03259948d073619ca9f2856f05568490ad6f264f
Zico4real228 <125082252+Zico4real228@users.noreply.github.com>
2023-02-19 15:10:30 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,8 @@
 * main - prints the alphabet in lowercase, and then in uppercase,
 * followed by a new line
 * Return: Always 0 (Success)
-*/int main(void)
+*/
+int main(void)
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
@@ -13,4 +14,3 @@ putchar(ch);
 putchar('\n');
 return (0);
 }
-
