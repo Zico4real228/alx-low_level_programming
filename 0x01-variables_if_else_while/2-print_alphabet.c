7005fb2a43e5c71891b7145f608b7dae94941cbf
Zico4real228 <zico4real22@yahoo.com>
2023-02-21 06:53:47 -0800

    2-print_alphabet.c 89082f3 0-putchar.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+<<<<<<< HEAD
 /**
 * main - prints the alphabet in lowercase,
 * followed by a new line
@@ -15,3 +16,37 @@ putchar('\n');
 return (0);
 }
 
+=======
+
+
+/**
+ *
+ *  * main - prints the alphabet in lowercase,
+ *
+ *   * followed by a new line
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	        char ch;
+
+
+		        for (ch = 'a'; ch <= 'z'; ch++)
+
+				        {
+
+						                putchar(ch);
+
+								        }
+
+			        putchar('\n');
+
+				        return (0);
+
+}
+>>>>>>> 9470654 (2-print_alphabet.c)
